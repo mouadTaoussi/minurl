e157c939e387aad7faa05bdd4c6407857a606a19
mouadtaoussi <mouadtaoussi0@gmail.com>
2020-09-19 15:01:54 +0100

    fourth commit

--- dist/url.config.js
+++ dist/url.config.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var config = {
     PORT: process.env.PORT || 8000,
-    database_connection: "mongodb://minurluser:minurluser@cluster0-shard-00-00.irglh.mongodb.net:27017,cluster0-shard-00-01.irglh.mongodb.net:27017,cluster0-shard-00-02.irglh.mongodb.net:27017/<dbname>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+    database_connection: ""
 };
 exports.default = config;
