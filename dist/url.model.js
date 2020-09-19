7bfd64072098787d62fdb53bd647a9cb2ba6b2cc
mouadtaoussi <mouadtaoussi0@gmail.com>
2020-09-19 14:55:54 +0100

    Remove dist

--- dist/url.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var URL = new mongoose_1.Schema({
-    name: { type: String, required: false },
-    url: { type: String, required: false },
-});
-var urlModel = mongoose_1.model("urls", URL);
-exports.default = urlModel;
