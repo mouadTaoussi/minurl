2657a173e50c0238aad4879c9eb08b02d9cfe09f
mouadtaoussi <mouadtaoussi0@gmail.com>
2020-09-20 15:44:28 +0100

    adding mongodb string connection

--- src/url.config.ts
+++ src/url.config.ts
@@ -5,7 +5,7 @@ interface Config {
 
 const config: Config = {
 	PORT : process.env.PORT || 8000,
-	database_connection : ""
+	database_connection : "mongodb://minurluser:minurluser@cluster0-shard-00-00.irglh.mongodb.net:27017,cluster0-shard-00-01.irglh.mongodb.net:27017,cluster0-shard-00-02.irglh.mongodb.net:27017/minurl?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 }
 
 export default config; 
